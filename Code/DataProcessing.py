3376d9db7b8438012a3258451a0be2781a75af97
babyfacebrian <bkalx202@gmail.com>
2019-10-25 16:16:37 -0400

    data cleaning

--- Code/DataProcessing.py
+++ Code/DataProcessing.py
@@ -1,8 +1,23 @@
 from typing import *
 import pandas as pd
-import numpy as np
 
 
 def clean_columns(raw_cols: List[str]) -> List[str]:
     return [name.lower().replace(' ', '_') for name in raw_cols]
+
+
+def fill_null_string_columns(data: pd.DataFrame, fill_dict: dict):
+    return data.fillna(value=fill_dict)
+
+
+# Removes all characters that are NOT: (letters numbers . ? , ! ')
+def clean_text_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    data[column_name] = data[column_name].str.replace('[^0-9a-zA-Z’ .!?,]', '', regex=True)
+    return data
+
+
+
+
+
+
 
