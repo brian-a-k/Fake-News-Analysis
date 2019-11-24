ac501c65754ae6869896c52354021e553a2a8962
babyfacebrian <bkalx202@gmail.com>
2019-11-24 18:02:09 -0500

    Spacy Text Categorizer training

--- Code/Testing/SpacyTesting.py
+++ Code/Testing/SpacyTesting.py
@@ -2,41 +2,20 @@ import spacy
 from typing import *
 from string import punctuation
 
-fake_headlines = [
-    'The    Amish In America Commit Their Vote   To Donald Trump Mathematically Guaranteeing Him A Presidential Victory     ABC News',
-    'Obama Signs Executive Order Declaring Investigation Into Election Results Revote Planned For Dec. 19th  ABC News',
-    'Comment on HALLOWEEN IN THE CASTRO RETURNS   IN 2014! by Day of the Dead 2015 History,   food and reflections      Andrea Lawson Gray']
 
-test = [' '.join(s.split()).lower() for s in fake_headlines]
+def assign_valid_class(row):
+    # sum of the REAL and Fake scores (they are not probabilities)
+    score_sum = (row.REAL + row.FAKE)
 
-for x in test:
-    print(x)
+    # divide each score by their sum for weighted probabilities
+    weighted_real = row.REAL / score_sum
+    weighted_fake = row.FAKE / score_sum
 
-
-
-# def run_ner_noun_parse(corpus: List[str]) -> List[str]:
-#     nlp = spacy.load('en_core_web_md')
-#     ner_pipeline = nlp.pipe(corpus)
-#     ner_types = ['PERSON', 'NORP', 'ORG', 'GPE', 'EVENT', 'WORK_OF_ART', 'LAW', 'LANGUAGE']  # NER types to filter
-#     noun_types = ['PROPN', 'NOUN']
-#
-#     # re-build the text corpus with just found entities within the NER types
-#     ner_corpus = []
-#     for doc in ner_pipeline:
-#         # Get all nouns and proper nouns
-#         noun_tokens = [token for token in doc if token.pos_ in noun_types]
-#
-#         # Get entities from nouns
-#         entity_tokens = [token for token in noun_tokens if token.ent_type_ in ner_types]
-#
-#         # Lemmatization of text (shortens it)
-#         lemma_tokens = [token.lemma_.strip().lower() for token in entity_tokens if token.is_stop is False
-#                         and token.text not in punctuation]
-#
-#         ner_corpus.append(' '.join(lemma_tokens))
-#     return ner_corpus
-#
-#
-# test = run_ner_noun_parse(fake_headlines)
-# for x in test:
-#     print(x)
+    if weighted_real > weighted_fake:
+        valid_class = 1
+    elif weighted_real < weighted_fake:
+        valid_class = 0
+    else:
+        # default to a real article if both weighted scores are equal (a lot of bias/vague articles in the data-set)
+        valid_class = 1
+    return valid_class
---
+++ Code/TrainTextCategorizer.py
@@ -0,0 +1,153 @@
+from typing import *
+import pandas as pd
+import numpy as np
+import spacy
+from spacy.util import minibatch, compounding
+from sklearn.model_selection import train_test_split
+
+
+def split_training_testing_data(data: pd.DataFrame, target: str, size: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    # Split data with sklearn train/test
+    x_train, x_test, y_train, y_test = train_test_split(data.drop(columns=[target]),
+                                                        data[target],
+                                                        test_size=size,
+                                                        random_state=21)
+
+    # concat x, y data-sets (for Spacy)
+    data_training = pd.concat([x_train, y_train], 1).reset_index(drop=True)
+    data_testing = pd.concat([x_test, y_test], 1).reset_index(drop=True)
+    return data_training, data_testing
+
+
+# Text Categorizer requires a iterable of tuples for training in the form of (text, score)
+def format_training_data(df: pd.DataFrame, feature: str, target: str, limit: int, split=0.8) -> Tuple[Any, Any]:
+    # Create training tuple list
+    df['training_content'] = df.apply(lambda row: (row[feature], row[target]), axis=1)
+    train_data_list = df['training_content'].tolist()
+
+    # Shuffle list
+    np.random.shuffle(train_data_list)
+    train_data_list = train_data_list[-limit:]
+
+    # Unpack into text and score labels
+    texts, labels = zip(*train_data_list)
+
+    # Text Cat. news labels
+    text_cat_labels = [{"REAL": bool(y), "FAKE": not bool(y)} for y in labels]
+
+    # Segment out data for cross validation while training
+    split = int(len(train_data_list) * split)
+    train_tuple = (texts[:split], text_cat_labels[:split])
+    eval_tuple = (texts[split:], text_cat_labels[split:])
+    return train_tuple, eval_tuple
+
+
+# Training Cross validation method
+def evaluate_textcat_training(tokenizer, textcat, texts, cats):
+    docs = (tokenizer(text) for text in texts)
+    tp = 1e-8  # True positives
+    fp = 1e-8  # False positives
+    fn = 1e-8  # False negatives
+    tn = 1e-8  # True negatives
+    for i, doc in enumerate(textcat.pipe(docs)):
+        gold = cats[i]
+        for label, score in doc.cats.items():
+            if label not in gold:
+                continue
+            if score >= 0.5 and gold[label] >= 0.5:
+                tp += 1.
+            elif score >= 0.5 and gold[label] < 0.5:
+                fp += 1.
+            elif score < 0.5 and gold[label] < 0.5:
+                tn += 1
+            elif score < 0.5 and gold[label] >= 0.5:
+                fn += 1
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f_score = 2 * (precision * recall) / (precision + recall)
+    accuracy = ((tp + tn) / (tp + tn + fp + fn))
+    return {'textcat_p': precision, 'textcat_r': recall, 'textcat_f': f_score, 'textcat_a': accuracy}
+
+
+# Make sure you have the spacy en_core_web_lg model downloaded
+def get_base_model():
+    nlp = spacy.load('en_core_web_lg')
+    if 'textcat' not in nlp.pipe_names:
+        textcat = nlp.create_pipe('textcat', config={'exclusive_classes': True, 'architecture': 'simple_cnn'})
+        nlp.add_pipe(textcat, last=True)
+    else:
+        textcat = nlp.get_pipe("textcat")
+    textcat.add_label("REAL")
+    textcat.add_label("FAKE")
+    return nlp
+
+
+def main(train_data: list, eval_texts: Tuple, eval_cat: Tuple, n_iterations: int, save_path: str, verbose: bool = True):
+    if save_path is None:
+        print('ERROR no save dir for model')
+        return
+
+    # main training loop
+    nlp_training = get_base_model()
+    other_pipes = [pipe for pipe in nlp_training.pipe_names if pipe != 'textcat']
+
+    with nlp_training.disable_pipes(*other_pipes):
+        optimizer = nlp_training.begin_training()
+        print('Training the model...')
+        for i in range(n_iterations):
+            loss_dict = {}
+            # batch up the examples using spaCy's minibatch
+            batches = minibatch(train_data, size=compounding(4.0, 32.0, 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp_training.update(texts, annotations, sgd=optimizer, drop=0.2, losses=loss_dict)
+
+            # Disable this if you do not want to see the CV scores every iteration (speeds this up)
+            if verbose:
+                textcat = nlp_training.get_pipe('textcat')
+                with textcat.model.use_params(optimizer.averages):
+                    scores = evaluate_textcat_training(nlp_training.tokenizer, textcat, eval_texts, eval_cat)
+                    # output the CV scores for each iteration
+                    print('Training Iteration: {}'.format(i + 1))
+                    print('{:^5}\t{:^5}\t{:^5}\t{:^5}\t{:^5}'.format('LOSS', 'P', 'R', 'F', 'A'))
+                    print('{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}\t{4:.3}\n'.format(loss_dict['textcat'],
+                                                                                scores['textcat_p'],
+                                                                                scores['textcat_r'],
+                                                                                scores['textcat_f'],
+                                                                                scores['textcat_a']))
+
+    # Save the trained model
+    if save_path is not None:
+        nlp_training.to_disk(save_path)
+        print('Training Complete!')
+        print('Model Saved To:', save_path)
+
+
+if __name__ == '__main__':
+    # Edit for your local file (the tokenized fake/real news csv)
+    load_path = '/Users/briankalinowski/Desktop/Data/sample_test.csv'
+
+    # Edit for where you want to save the trained model
+    save_model_path = '/Users/briankalinowski/Desktop/Data/spacy/test_model'
+
+    # Tokenized DataFrame
+    data = pd.read_csv(load_path)
+
+    # Train/Test split
+    training_raw, testing_raw = split_training_testing_data(data, 'valid_score', 0.5)
+
+    # Length (rows) of the data
+    n_texts = training_raw.shape[0]
+
+    # Format the training data
+    (train_texts, train_cats), (dev_texts, dev_cats) = format_training_data(training_raw, 'tokenized_content',
+                                                                            'valid_score', limit=n_texts)
+
+    # Combine training text and labels
+    train_final = list(zip(train_texts, [{'cats': cats} for cats in train_cats]))
+    print('Loading news data...')
+    print('Using {} examples ({} for training, {} for cross evaluation)\n'.format(n_texts, len(train_texts),
+                                                                                  len(dev_texts)))
+
+    # Train the model
+    main(train_final, dev_texts, dev_cats, n_iterations=5, save_path=save_model_path)
